e37aedaa6dcb999201800fac52698400af069e07
jmsandiegoo <jmsandiego0710@gmail.com>
2024-03-10 18:10:04 +0800

    Remove node_modules and dist in git tracking

--- dist/database/Database.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class default_1 {
-}
-exports.default_1 = default_1;
---
+++ src/constants/messages.ts
@@ -0,0 +1,3 @@
+
+export const initialize_message: string = "Initializing...";
+export const greet_message: string = "Welcome to the Festive Gift Redemption System! It's the season of giving, and we're here to assist you \n in distributing joy and gifts to the teams";
