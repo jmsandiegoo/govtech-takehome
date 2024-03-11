fbbd98a61eb36d1465e1526c8f44faa7c4f9e73e
jmsandiegoo <jmsandiego0710@gmail.com>
2024-03-11 17:01:44 +0800

    Add domain models

---
+++ src/models/Redeem.ts
@@ -0,0 +1,6 @@
+
+export interface Redeem {
+    redeem_id: string,
+    team_name: string,
+    redeemed_at: Date | null,
+}
---
+++ src/models/Staff.ts
@@ -0,0 +1,6 @@
+
+export interface Staff {
+    staffPassId: string;
+    teamName: string;
+    createdAt: Date | null;
+}
