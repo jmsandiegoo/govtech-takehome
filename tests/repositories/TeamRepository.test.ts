32389152c93827611670a6c25a3caf3191ee7edc
jmsandiegoo <jmsandiego0710@gmail.com>
2024-03-12 14:36:01 +0800

    Add TeamRepository unit test

---
+++ tests/repositories/TeamRepository.test.ts
@@ -0,0 +1,43 @@
+import { Database } from "../../src/database/Database";
+import { TeamRepository } from "../../src/repositories/TeamRepository";
+
+describe("Staff Repository", () => {
+    let teamRepo: TeamRepository;
+    let dbService: Database;
+
+    beforeAll(() => {
+        dbService = new Database();
+        teamRepo = new TeamRepository(dbService);
+    });
+
+    afterAll(() => {
+        dbService.dbDestroy();
+    })
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+    })
+
+    test("getTeamByName returns a team object when team name exists", async () => {
+        const teamName = "BASS";
+
+        const mockTeamData = {
+            team_name: "BASS", 
+            created_at: new Date("2022-01-01"),
+        };
+
+        const team = await teamRepo.getTeamByName(teamName);
+
+        expect(team).toEqual({
+            teamName: mockTeamData.team_name,
+            createdAt: expect.any(Date),
+        });
+    });
+
+    test("getStaffByPassId returns a null object when team name does not exists", async () => {
+        const teamName = "NON_EXISTENT_ID";
+
+        const team = await teamRepo.getTeamByName(teamName);
+        expect(team).toBeNull();
+    });
+});
